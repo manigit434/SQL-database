e2caf9cfeb576158fed1f012e747fae192dbe2fc
Manikanta Katta <kattamanikanta434@gmail.com>
2026-03-04 18:30:31 +0530

    Update and rename 180. Consecutive Numbers to easy/180. Consecutive Numbers.sql

---
+++ easy/180. Consecutive Numbers.sql
@@ -0,0 +1,15 @@
+-- ==================================================
+-- Problem: 180. Consecutive Numbers
+-- Link: https://leetcode.com/problems/consecutive-numbers/
+-- Difficulty: Easy
+-- Concepts: Self JOIN, Consecutive Rows, DISTINCT
+-- ==================================================
+
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 
+    ON l1.id = l2.id - 1
+JOIN Logs l3 
+    ON l2.id = l3.id - 1
+WHERE l1.num = l2.num
+  AND l2.num = l3.num;
